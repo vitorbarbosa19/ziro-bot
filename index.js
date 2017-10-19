c6e79c393784e68f739dc2bc769e18d01f38dde5
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-10-18 19:28:11 -0700

    deploying with now instead of google cloud

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const request = require('request')
 
-exports.ziroBot = function ziroBot (req, res) {
+module.exports = (req, res) => {
 	request('https://www.instagram.com/ziromoda/media/?max_id=1', function(error, response, body) {
-		const images = JSON.parse(body).items.map( function(post) {
+		const arrayOfImagesUrls = JSON.parse(body).items.map( function(post) {
 			return post.images.standard_resolution.url
 		})
-		res.send(images)
+		res.end(JSON.stringify(arrayOfImagesUrls))
 	})
 }
-
-if (module === require.main) {
-	const server = ziroBot.listen(process.env.PORT || 8081, function() {
-		console.log(`Ziro Bot is listening on port ${server.address().port}`)
-	})
-}
-
-// ziroBot.listen(8080, function() {
-// 	console.log('Ziro Bot is listening on address http://localhost:8080')
-// })
