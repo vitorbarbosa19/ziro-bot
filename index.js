6e6708c63e5d6c39a788b08884ff04a19b8e5e01
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-12-03 10:09:54 -0800

    added handler to route requests

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
-const express = require('express')
-const request = require('request')
+const { send } = require('micro')
 
-const brand = {
-	name: 'luzia',
-	url: 'https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/23507546_970353509784421_6944867946179067904_n.jpg'
+module.exports = async (req, res) => {
+	try {
+		let response
+		switch (req.url) {
+			case '/scrape':
+				const scrapeAccount = require('./functions/scrapeAccount')
+				response = await scrapeAccount(req)
+			case '/download':
+				const downloadImageUrl = require('./functions/downloadImageUrl')
+				response = await downloadImageUrl(req)
+			default:
+				response = '404. Rota não encontrada.'
+				break
+		}
+		send(res, 200, response)
+	} 
+	catch (error) {
+		console.log(error)
+		send(res, 500, error)
+	}
 }
 
-const igAccount = 'https://instagram.com/nuxxoficial'
+// const express = require('express')
+// const request = require('request')
 
-const app = express()
+// const brand = {
+// 	name: 'luzia',
+// 	url: 'https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/23507546_970353509784421_6944867946179067904_n.jpg'
+// }
 
-app.get('/', (req, res) => {
-	res.attachment(`${brand.name}.jpg`)
-	request(brand.url).pipe(res)
-})
+// const igAccount = 'https://instagram.com/nuxxoficial'
 
-app.get('/scraper', async (req, res) => {
-	const scrapeAccount = require('./functions/scrapeAccount')
-	anchorTagsHrefs = await scrapeAccount(igAccount)
-	const scrapeImagePage = require('./functions/scrapeImagePage')
-	imageSrcs = await scrapeImagePage(anchorTagsHrefs)
-	console.log(imageSrcs)
-	res.send(imageSrcs)
-})
+// const app = express()
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+// app.get('/', (req, res) => {
+// 	res.attachment(`${brand.name}.jpg`)
+// 	request(brand.url).pipe(res)
+// })
+
+// app.get('/scraper', async (req, res) => {
+// 	const scrapeAccount = require('./functions/scrapeAccount')
+// 	anchorTagsHrefs = await scrapeAccount(igAccount)
+// 	const scrapeImagePage = require('./functions/scrapeImagePage')
+// 	imageSrcs = await scrapeImagePage(anchorTagsHrefs)
+// 	console.log(imageSrcs)
+// 	res.send(imageSrcs)
+// })
+
+// app.listen(3000, () => console.log('Listening on port 3000'))
