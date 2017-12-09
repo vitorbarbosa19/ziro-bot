f7cd4985f1f559901c2602a092ebee1291cbcb75
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-12-08 16:44:09 -0800

    added function that scrapes the published date of the images, so we can skip them if older than our last download date

--- functions/scrapeImagePage.js
+++ functions/scrapeImagePage.js
@@ -3,14 +3,20 @@ const scrapeImagePage = async (anchorTagsHrefs, account) => {
 	const puppeteer = require('puppeteer')
 	const browser = await puppeteer.launch({ timeout: 60000 })
 	const page = await browser.newPage()
-	// scrape from each image page the image src url
+	// scrape from each image page the image src url if the image is more recent than the last download
 	const request = require('request')
 	const fs = require('fs')
 	for (let index = 0; index < anchorTagsHrefs.length; index++) {
 		await page.goto(anchorTagsHrefs[index], { timeout: 60000 })
+		// scrape the published date of the image, so we can skip it if not newer than our last download date
+		const times = await page.$$eval('time', (timeTags) => {
+			return Array.prototype.map.call(timeTags, (time) => time.getAttribute('datetime'))
+		})
+		// scrape images src on the page. Will bring the logo's and the desired image's
 		let imagesSrcs = await page.$$eval('img', (imageTags) => {
 			return Array.prototype.map.call(imageTags, (img) => img.src)
 		})
+		// filter away the logo src, leaving only the desired image src
 		let imageSrc = imagesSrcs.filter( (src) => {
 			return /\/e35\//.test(src)
 		})
