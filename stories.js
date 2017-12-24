72e4d878f24ec673af76599c226b96d8014b9743
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-12-23 19:02:06 -0800

    added functionalities for login and to get cookies

--- stories.js
+++ stories.js
@@ -8,18 +8,38 @@ puppeteer.launch().then( async (browser) => {
 		await page.type(`input[type*='text']`, 'ma.joana_', {delay: 200})
 		await page.type(`input[type*='password']`, 'casa10', {delay: 200})
 		await page.click(`form > span > button`)
-		await page.waitFor(1500)
-		await page.screenshot({ path: './images.jpg' })
+		await page.waitForNavigation()
+		const loginChallenge = await page.$$eval('button', (buttonTags) => {
+			return Array.prototype.map.call(buttonTags, (button) => { 
+				return button.textContent === 'Send Security Code' ? true : false
+			}).reduce( (accumulator, currentValue) => {
+				return accumulator || currentValue
+			})
+		})
+		if (loginChallenge) {
+			await page.screenshot({ path: './screenshots/preChallenge.jpg' })
+			await page.click(`form > span`)
+			await page.click(`form > span`)
+			await page.waitFor(1500)
+			await page.screenshot({ path: './screenshots/postSecurityCodeRequest.jpg' })
+			const askUserForSecurityCode = require('./askUserForSecurityCode')
+			const securityCode = await askUserForSecurityCode()
+			await page.type(`#security_code`, securityCode, {delay: 200})	
+			await page.click(`form > span`)
+			await page.waitForNavigation()
+			await page.screenshot({ path: './screenshots/postChallenge.jpg' })
+		}
+		console.log(await page.cookies())
+		await browser.close()
 		// const inputTags = await page.$$eval('input', (inputTags) => {
 		// 	return Array.prototype.map.call(inputTags, input => input.placeholder)
 		// })
 		// console.log(inputTags)
-		await browser.close()
+		
 	} catch (error) {
 		console.log(error)
 	}
 })
-
 
 // Poema Hit Stories feed
 // https://i.instagram.com/api/v1/feed/user/198255331/story/
