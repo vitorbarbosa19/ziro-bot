57aabd183113ce8bb0c50ecae05093af44bd987f
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2018-01-01 16:53:53 -0800

    separate files from stories downloader into its own repository

--- getFromCookieFile.js
+++
@@ -1,9 +0,0 @@
-const getFromCookieFile = (cookieToRetrieve) => {
-	const cookies = require('./cookies.js')
-	const cookieRetrieved = cookies.filter( (cookie) => {
-		return cookie.name === cookieToRetrieve
-	})
-	return cookieRetrieved.pop().value
-}
-
-module.exports = getFromCookieFile
